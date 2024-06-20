9aa611a77683b0eade7e26070f4db3361851d973
Sofyan Egi <sofyanegil@gmail.com>
2024-06-20 16:53:05 +0700

    manage users

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,6 +7,9 @@ import Home from '../views/home/index.jsx';
 import Register from '../views/auth/register.jsx';
 import Login from '../views/auth/login.jsx';
 import Dashboard from '../views/admin/dashboard/index.jsx';
+import UsersIndex from '../views/admin/users/index.jsx';
+import UsersCreate from '../views/admin/users/create.jsx';
+import UsersEdit from '../views/admin/users/edit.jsx';
 
 export default function AppRoutes() {
   const { isAuthenticated } = useContext(AuthContext);
@@ -17,6 +20,9 @@ export default function AppRoutes() {
       <Route path="/register" element={isAuthenticated ? <Navigate to="/admin/dashboard" replace /> : <Register />} />
       <Route path="/login" element={isAuthenticated ? <Navigate to="/admin/dashboard" replace /> : <Login />} />
       <Route path="/admin/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />} />
+      <Route path="/admin/users" element={isAuthenticated ? <UsersIndex /> : <Navigate to="/login" replace />} />
+      <Route path="/admin/users/create" element={isAuthenticated ? <UsersCreate /> : <Navigate to="/login" replace />} />
+      <Route path="/admin/users/edit/:id" element={isAuthenticated ? <UsersEdit /> : <Navigate to="/login" replace />} />
     </Routes>
   );
 }
