7b70a629379653c46241e404b577b0154f6c295d
Sofyan Egi <sofyanegil@gmail.com>
2024-06-20 08:15:06 +0700

    install axios, js-cookie, react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+//import BrowserRouter dari react router
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
